cfe65cf48740ef5458379fa11dd0c6768c8ef7fe
junw <wo1261931780@gmail.com>
2025-12-30 13:02:02 +0800

    初始化

---
+++ src/main/java/wo1261931780/accountManage/AccountManageApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.accountManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/accountManage/AccountManageApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.accountManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
